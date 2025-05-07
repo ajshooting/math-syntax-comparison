94bd6a5b012b1aeee253b77c93ffb4f345848ead
ajshooting <ajshooting.aj@gmail.com>
2025-05-07 17:02:13 +0900

    config...?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import remarkGfm from 'remark-gfm';
+
+
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [mdx()],
+    markdown: {
+        remarkPlugins: [remarkMath, remarkGfm],
+        rehypePlugins: [[rehypeKatex, { displayMode: true }]],
+        syntaxHighlight: 'shiki',
+        shikiConfig: {
+            theme: 'css-variables',
+            wrap: true,
+        },
+    },
+});
